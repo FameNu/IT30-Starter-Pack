1f7367a0cb26ad3dca5aa6b32f641f2308cbd475
FameNu <110712106+FameNu@users.noreply.github.com>
2024-06-25 00:30:30 +0700

    add : bg but hide it by comment

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,14 @@ import SITAndStarter from "@/components/ourBrand/SITAndStarter.vue";
 </script>
 
 <template>
-  <div class="bg-main-bg bg-no-repeat bg-center bg-cover">
+  <!-- <div class="bg-main-bg bg-no-repeat bg-bottom bg-cover"> -->
+  <div>
     <header class="absolute top-0 w-full">
       <SITAndStarter />
     </header>
+    <!-- <div class=" absolute w-full h-full">
+      <img src="/bg/Banner-GoogleForm.png" alt="main-bg-image" class="w-full h-full ins">
+    </div> -->
     <main class="w-screen h-screen p-10 pt-24">
       <RouterView />
     </main>
