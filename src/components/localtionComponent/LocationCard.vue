689f9de5b3a6ef5f97ed745f44d7399a005fd351
BomScoob12 <sorawitkraukham@gmail.com>
2024-06-25 21:48:29 +0700

    feat: Location view

---
+++ src/components/localtionComponent/LocationCard.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props = defineProps({
+  pathOfImg: String,
+  nameOfImage: {
+    type: String,
+    default: 'รูปภาพ',
+  },
+});
+</script>
+
+<template>
+  <div class="flex-col p-4 mx-3 my-2 justify-center w-1/3">
+    <img :src="props.pathOfImg" :alt="nameOfImage"/>
+    <section class="flex-col pl-3">
+      <div class="font-bold mt-4">
+        <slot name="name">Name of location</slot>
+      </div>
+      <div class="font-light text-clip">
+        <slot name="description">Description of location</slot>
+      </div>
+    </section>
+  </div>
+</template>
---
+++ src/views/LocaltionView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import LocationCard from '@/components/localtionComponent/LocationCard.vue';
+const pathOfImgTest: string = "public/bg/bg7.png"
+</script>
+
+<template>
+  <div class="w-full h-full">
+    <LocationCard :pathOfImg="pathOfImgTest" nameOfImage="รูปไรวะเนี่ย"/>
+  </div>
+</template>
+
+<style scoped></style>
