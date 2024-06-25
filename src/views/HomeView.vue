3ac0e7f2de9ee514ae8eaed4927ca4492642b19b
BomScoob12 <sorawitkraukham@gmail.com>
2024-06-25 20:37:10 +0700

    Add: config daisyui, and adding IG icon to home page.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import CardList from "@/components/homeComponent/CardList.vue";
+import IgIcon from "@/components/icons/IgIcon.vue";
 const cardData = [
   {
     name: "Action-1",
@@ -38,5 +39,9 @@ const cardData = [
       <h2>Where you want to visit?</h2>
     </div>
     <CardList :card-data="cardData" />
+  </div>
+  <div class="flex absolute bottom-5 right-5">
+    <IgIcon color="green" size="h-12" class="mx-3"/>
+    <IgIcon color="black" size="h-12" class="mx-3"/>
   </div>
 </template>
