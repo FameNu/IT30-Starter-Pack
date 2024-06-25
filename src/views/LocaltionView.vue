8d1b0f4f17d6997e65356c6efb4d0b7309d2fb0d
BomScoob12 <sorawitkraukham@gmail.com>
2024-06-25 22:11:32 +0700

    add : template image their hover

--- src/views/LocaltionView.vue
+++ src/views/LocaltionView.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
 import LocationCard from '@/components/localtionComponent/LocationCard.vue';
-const pathOfImgTest: string = "public/bg/bg7.png"
+const pathOfImgTest: string = 'public/bg/bg7.png';
 </script>
 
 <template>
-  <div class="w-full h-full flex flex-wrap">
-    <LocationCard :pathOfImg="pathOfImgTest" nameOfImage="รูปไรวะเนี่ย"/>
-    <LocationCard :pathOfImg="pathOfImgTest" nameOfImage="รูปไรวะเนี่ย"/>
+  <div class="w-screen h-screen flex">
+    <section class="w-2/3">
+      <LocationCard :pathOfImg="pathOfImgTest" nameOfImage="รูปไรวะเนี่ย" />
+      <LocationCard :pathOfImg="pathOfImgTest" nameOfImage="รูปไรวะเนี่ย" />
+    </section>
+    <section class="w-1/3">
+      <!-- Image base on thier hover -->
+       <div>Testing image</div>
+      <img src="" alt="" srcset="" />
+    </section>
   </div>
 </template>
 
