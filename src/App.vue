832a09539f54b682a02e10d7b770406c8be5f02e
FameNu <110712106+FameNu@users.noreply.github.com>
2024-06-26 20:35:26 +0700

    refactor: Update main background image size and positioning

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <script setup lang="ts">
 import SITAndStarter from '@/components/ourBrand/SITAndStarter.vue'
+
+// class image
+// bg-main-bg bg-cover bg-no-repeat bg-fixed h-fit
 </script>
 
 <template>
-  <div>
-    <div class="-z-10 absolute w-full h-full">
-      <img src="/bg/bg6.png" alt="main-bg-image" class="w-full fixed -bottom-4">
-    </div>
-    <header class="fixed z-50 top-0 w-full">
-      <SITAndStarter />
-    </header>
-    <main class="w-screen h-screen p-10 pt-24">
-      <RouterView />
-    </main>
+  <header class="fixed z-50 top-0 w-full">
+    <SITAndStarter />
+  </header>
+  <main class="w-screen min-h-screen p-10 pt-24">
+    <RouterView />
+  </main>
+  <div class="-z-10 absolute top-0 w-screen h-screen">
+    <img src="/bg/bg6.png" alt="main-bg-image" class="max-lg:h-screen w-dvw md:w-full fixed lg:-bottom-4" />
   </div>
 </template>
 
