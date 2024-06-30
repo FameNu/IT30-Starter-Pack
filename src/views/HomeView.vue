c3fe9bfa7cfbe7aca12688cf10e8bde8c5b03fda
FameNu <phuwamet.fame@gmail.com>
2024-07-01 02:41:58 +0700

    modify: - mockup data - remove setting of icon

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,36 +3,33 @@ import CardList from '@/components/homeComponent/CardList.vue'
 import IgIcon from '@/components/icons/IgIcon.vue'
 const cardData = [
   {
-    name: 'Action-1',
-    image: '/mascot/Mascot-Action-1.png',
-    island: '/sky-island.png'
+    name: 'Sproutbara',
+    image: '/mascot/sproutbara.png'
   },
   {
-    name: 'Action-2',
-    image: '/mascot/Mascot-Action-2.png',
-    island: '/sky-island.png'
+    name: 'Sunnybara',
+    image: '/mascot/sunnybara.png'
   },
   {
-    name: 'Action-3',
-    image: '/mascot/Mascot-Action-3.png',
-    island: '/sky-island.png'
+    name: 'Rainybara',
+    image: '/mascot/rainybara.png'
   },
   {
-    name: 'Action-4',
-    image: '/mascot/Mascot-Action-4.png',
-    island: '/sky-island.png',
+    name: 'Russetbara',
+    image: '/mascot/russetbara.png'
   },
   {
-    name: 'Action-5',
-    image: '/mascot/Mascot-Action-5.png',
-    island: '/sky-island.png',
+    name: 'Cozybara',
+    image: '/mascot/cozybara.png'
   }
 ]
 </script>
 
 <template>
   <div class="h-full flex flex-col lg:justify-center items-center gap-16">
-    <div class="bg-yellow-300 w-fit py-4 px-10 mt-2 mx-auto rounded-xl flex flex-col items-center shadow-xl">
+    <div
+      class="bg-yellow-300 w-fit py-4 px-10 mt-2 mx-auto rounded-xl flex flex-col items-center shadow-xl"
+    >
       <h1 class="text-header-desktop">Welcome to Capy Mellow Land</h1>
       <h2 class="text-header-desktop">Where you want to visit?</h2>
     </div>
@@ -40,6 +37,6 @@ const cardData = [
   </div>
   <div class="flex absolute bottom-5 right-5">
     <IgIcon @click="$router.push('locations')" color="green" size="h-12" class="mx-3" />
-    <IgIcon color="black" size="h-12" class="mx-3" />
+    <IgIcon class="mx-3" />
   </div>
 </template>
