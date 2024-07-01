7800206eebbe1e9e861364ebbed51fff6b64fdc3
FameNu <110712106+FameNu@users.noreply.github.com>
2024-07-01 22:04:24 +0700

    feat: Add MapIcon component with SVG map image

---
+++ src/components/icons/MapIcon.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import MenuIcon from '@/components/icons/MenuIcon.vue'
+import MapSVG from '@/assets/svg/mapSVG.vue'
+</script>
+
+<template>
+  <MenuIcon>
+    <template>
+      <MapSVG />
+    </template>
+  </MenuIcon>
+</template>
