c441ec5da4250e5e391539459485594605f55211
FameNu <110712106+FameNu@users.noreply.github.com>
2024-07-02 01:17:08 +0700

    feat: Add SVG icons for bar and x mark

---
+++ src/components/icons/BarMenuIcon.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import MenuIcon from '@/components/icons/MenuIcon.vue'
+import barSVG from '@/assets/svg/barSVG.vue'
+defineProps({
+  color: {
+    type: String,
+    default: 'white'
+  }
+})
+</script>
+
+<template>
+  <MenuIcon>
+    <template #default>
+      <barSVG :color="color" />
+    </template>
+  </MenuIcon>
+</template>
---
+++ src/components/icons/XMarkIcon.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import MenuIcon from '@/components/icons/MenuIcon.vue'
+import xMarkSVG from '@/assets/svg/xMarkSVG.vue'
+defineProps({
+  color: {
+    type: String,
+    default: 'white'
+  }
+})
+</script>
+
+<template>
+  <MenuIcon>
+    <template #default>
+      <xMarkSVG :color="color" />
+    </template>
+  </MenuIcon>
+</template>
