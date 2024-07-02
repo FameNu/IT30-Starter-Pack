69f9db56572e0bcc87795572644947bbe6580127
Tonnamkit <kittipat.bual@mail.kmutt.ac.th>
2024-07-02 15:08:41 +0700

    Add : VITE_BASE_URL as BASE_URL and fetch messages

--- src/views/BoardView.vue
+++ src/views/BoardView.vue
@@ -1,12 +1,35 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import { io } from 'socket.io-client';
+
+const BASE_URL = import.meta.env.VITE_BASE_URL
+const socket = io(BASE_URL);
+
 
 const messages = ref<string[]>([]);
+const newMessage = ref('');
+
+const fetchMessages = async () => {
+    const response = await fetch(`${BASE_URL}/api/messages`);
+    const data = await response.json();
+    messages.value = data.data.map((message: { attributes: { message: string; }; }) => message.attributes.message)
+} 
+
+onMounted(() => {
+  socket.on('connect', () => {
+    console.log('Connected to server:', socket.id);
+    fetchMessages();   
+  });
+  console.log(import.meta.env.BASE_URL)
+});
+
 const addMessage = (): void => {
   const newMessage = prompt("Enter your message:");
   if (newMessage) {
     messages.value.push(newMessage);
   }
+
+  
 };
 </script>
 
