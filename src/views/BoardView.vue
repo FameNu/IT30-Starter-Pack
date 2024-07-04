9f5abddd5589632837694b7d5dc3e6e69bf983ed
Tonnamkit <kittipat.bual@mail.kmutt.ac.th>
2024-07-04 18:29:35 +0700

    add : Form with modal, implement with socket

--- src/views/BoardView.vue
+++ src/views/BoardView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import Form from "@/components/boardComponent/Form.vue";
 import { ref, onMounted } from 'vue';
 import { io } from 'socket.io-client';
 
@@ -7,9 +8,9 @@ const socket = io(BASE_URL);
 
 
 const messages = ref<string[]>([]);
-const newMessage = ref('');
+const isModalOpen = ref(false);
 
-const fetchMessages = async () => {
+const initMessages = async () => {
     const response = await fetch(`${BASE_URL}/api/messages`);
     const data = await response.json();
     messages.value = data.data.map((message: { attributes: { message: string; }; }) => message.attributes.message)
@@ -18,31 +19,59 @@ const fetchMessages = async () => {
 onMounted(() => {
   socket.on('connect', () => {
     console.log('Connected to server:', socket.id);
-    fetchMessages();   
+    initMessages();   
   });
   console.log(import.meta.env.BASE_URL)
 });
 
-const addMessage = (): void => {
-  const newMessage = prompt("Enter your message:");
-  if (newMessage) {
-    messages.value.push(newMessage);
-  }
+const addMessage = async (newMessage: string) => {
+  const newMessageObj = { data: { message: newMessage } };
+  try {
+      const response = await fetch(`${BASE_URL}/api/messages`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newMessageObj),
+      });
 
-  
+      const result = await response.json();
+
+      socket.emit('sendMessages', result.data.attributes.message);
+    }catch (error) {
+      console.error('Error Add new messages:', error);
+    }
+};
+
+socket.on('recvMessages', (newMessage) => {
+  messages.value.push(newMessage);
+});
+
+const openModal = () => {
+  isModalOpen.value = true;
+};
+
+const closeModal = () => {
+  isModalOpen.value = false;
+};
+
+const handleModalSubmit = (message: string) => {
+  addMessage(message);
+  closeModal();
 };
 </script>
 
 <template>
     <div class="p-4">
       <div class="mb-4">
-        <button @click="addMessage" class="bg-blue-500 text-white py-2 px-4 rounded">Add Message</button>
+        <button @click="openModal" class="bg-blue-500 text-white py-2 px-4 rounded">Add Message</button>
       </div>
       <div class="space-y-2">
         <div v-for="(message, index) in messages" :key="index" class="bg-gray-100 p-4 rounded shadow">
           {{ message }}
         </div>
       </div>
+      <Form :show="isModalOpen" @close="closeModal" @submit="handleModalSubmit" />
     </div>
   </template>
 
