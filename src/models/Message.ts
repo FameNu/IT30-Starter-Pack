3fadeebac5c44240ef539b518913d6cf06018c26
Tonnamkit <kittipat.bual@mail.kmutt.ac.th>
2024-07-14 12:35:08 +0700

    update : MessageObj type and addd CommentObj

--- src/models/Message.ts
+++ src/models/Message.ts
@@ -1,7 +1,13 @@
+interface CommentObj {
+    id : number
+    comment : string
+}
+
 interface MessageObj {
     id : number
     message: string;
     landName: string;
+    comments: CommentObj[]
 }
 
-export { type MessageObj }
+export { type MessageObj, type CommentObj }
